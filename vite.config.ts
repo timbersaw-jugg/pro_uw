738817af3eb1568212143929162aff349cd6dc63
timbersaw-jugg <anil.kumar87654321@gmail.com>
2026-03-10 20:44:56 +0530

    allow render host

--- vite.config.ts
+++ vite.config.ts
@@ -21,7 +21,7 @@ export default defineConfig(({ mode }) => {
       // HMR is disabled in AI Studio via DISABLE_HMR env var.
       // Do not modifyâfile watching is disabled to prevent flickering during agent edits.
       hmr: process.env.DISABLE_HMR !== 'true',
-      allowedHosts: true
+      allowedHosts: ["pro-uw-1.onrender.com"]
     },
   };
 });
